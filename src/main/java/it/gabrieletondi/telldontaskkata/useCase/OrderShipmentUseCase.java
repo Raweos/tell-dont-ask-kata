24c52806be935ff474e1fddeec3e4d8da2f0c167
raweos <kosa.095@gmail.com>
2018-09-04 19:02:11 +0200

    Refactoring

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderShipmentUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderShipmentUseCase.java
@@ -1,24 +1,19 @@
 package it.gabrieletondi.telldontaskkata.useCase;
 
 import it.gabrieletondi.telldontaskkata.domain.Order;
-import it.gabrieletondi.telldontaskkata.domain.OrderStatus;
 import it.gabrieletondi.telldontaskkata.repository.OrderRepository;
 import it.gabrieletondi.telldontaskkata.service.ShipmentService;
 
-import static it.gabrieletondi.telldontaskkata.domain.OrderStatus.CREATED;
-import static it.gabrieletondi.telldontaskkata.domain.OrderStatus.REJECTED;
-import static it.gabrieletondi.telldontaskkata.domain.OrderStatus.SHIPPED;
-
-public class OrderShipmentUseCase {
+class OrderShipmentUseCase {
     private final OrderRepository orderRepository;
     private final ShipmentService shipmentService;
 
-    public OrderShipmentUseCase(OrderRepository orderRepository, ShipmentService shipmentService) {
+    OrderShipmentUseCase(OrderRepository orderRepository, ShipmentService shipmentService) {
         this.orderRepository = orderRepository;
         this.shipmentService = shipmentService;
     }
 
-    public void run(OrderShipmentRequest request) {
+    void run(OrderShipmentRequest request) {
         final Order order = orderRepository.getById(request.getOrderId());
         order.orderShipment(shipmentService);
         orderRepository.save(order);
